23e231420db08b8df572ca28b2cc1c0f68beddf4
cpolychroniadis <cpolychroniadis@yahoo.com>
2025-02-19 16:48:41 -0500

    changing why us

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { AboutUsComponent } from "../about-us/about-us.component";
+import { Component, OnInit,OnDestroy  } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
-  imports: [AboutUsComponent],
+  standalone: true,
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit  {
+export class HomeComponent implements OnInit, OnDestroy  {
+   currentImageIndex;
+   images: string[] = ['mcentire.webp', 'truck_dock.webp', 'dock_port600.webp'];
+   content = [
+    {
+      title: 'Our Story',
+      description: ' In business since 1938, McEntire Produce has grown to become the largest, family-owned, fresh produce processor in the Southeast. Located in a state-of-the art facility in Columbia, South Carolina, we are committed to providing the freshest, safest and most sustainably processed produce available to the market today. '
+    },
+    {
+      title: 'McEntire Trucking',
+      description: 'We operate state-of-the-art facilities equipped with the latest technology to ensure the highest quality and safety standards...'
+    },
+    {
+      title: 'MLX Freight',
+      description: 'We are committed to sustainable practices that minimize our environmental impact and promote responsible sourcing...'
+    }
+  ];
+   intervalId: any; 
 
-   myImages: string[] = [ "mcentire.webp", "truck_dock.webp", "port_containers.webp"];
-   currentImageIndex = 0;
+   constructor() {  this.currentImageIndex = 0;    }
 
    ngOnInit() {
-    setInterval(() => {
-      this.currentImageIndex = (this.currentImageIndex + 1) % this.myImages.length;
-    }, 3000); // 3000 milliseconds = 3 seconds
+    // Initialization logic here
+    this.currentImageIndex = 1;
+   }
+ 
+   ngOnDestroy() {
+     // Clear the interval when the component is destroyed to avoid memory leaks
+     if (this.intervalId) {
+       clearInterval(this.intervalId);
+     }
+   }
+
+  changeContent() {
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
   }
+  
 
 }
